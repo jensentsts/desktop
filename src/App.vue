7c1282ce3e5582f599be5eb60482416d77f30b11
jensentsts <jensentsts@163.com>
2025-09-11 01:11:46 +0800

    能用

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,39 @@
 <script setup lang="ts">
 import { reactive, ref } from 'vue'
 import CircleProgress from './circle_progress.vue'
+import Barrage from './barrage.vue'
 
-var h = ref(0)
-var m = ref(0)
-var s = ref(0)
+// 时间
+var time = ref({
+  h: 0,
+  m: 0,
+  s: 0,
+})
 
-var bg = ref("url('https://t.alcy.cc/fj')")
+// 背景图片
+// var bg = ref("url('https://t.alcy.cc/fj')")
+var bg = ref("url('')")
 
+// 时间更新
 setInterval(() => {
   var date = new Date()
-  h.value = date.getHours()
-  m.value = date.getMinutes()
-  s.value = date.getSeconds()
+  time.value.h = date.getHours()
+  time.value.m = date.getMinutes()
+  time.value.s = date.getSeconds()
 }, 1000)
 </script>
 
 <template>
-  <div class="underlay" :style="{ '--bg-url': bg }">
-    <circle-progress id="hour" :value="h" :valueMax="24" />
-    <circle-progress id="minute" :value="m" :valueMax="60" />
-    <circle-progress id="second" :value="s" :valueMax="60" />
+  <div class="underlay bg" :style="{ '--bg-url': bg }">
+    <circle-progress id="hour" :value="time.h" :valueMax="24" />
+    <circle-progress id="minute" :value="time.m" :valueMax="60" />
+    <circle-progress id="second" :value="time.s" :valueMax="60" />
   </div>
+  <barrage class="underlay" />
 </template>
 
 <style scoped>
 .underlay {
-  --bg-url: '';
-
   position: fixed;
   top: 0;
   left: 0;
@@ -36,12 +42,24 @@ setInterval(() => {
   display: flex;
   justify-content: center;
   align-items: center;
+  overflow: hidden;
+}
+
+.bg {
+  --bg-url: '';
   background-color: #000030;
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
   background-image: var(--bg-url);
-  overflow: hidden;
   z-index: -9999;
+}
+
+barrage {
+  z-index: 0;
+}
+
+circle-progress {
+  z-index: 1;
 }
 </style>
---
+++ src/barrage.vue
@@ -0,0 +1,228 @@
+<script setup>
+import { reactive, ref } from 'vue'
+
+class Barrage {
+  text = ''
+  onShow = false
+  constructor(text = '', onShow = false) {
+    this.text = text
+    this.onShow = onShow
+  }
+}
+
+class BarrageTrack {
+  id = ''
+  barrages = reactive([])
+  barrage_waiting = 0
+  constructor(id = '') {
+    this.id = id
+    this.barrages = reactive([])
+    this.barrage_waiting = 0
+  }
+
+  sendBarrage(barrageIndex) {
+    this.barrage_waiting--
+    if (barrageList[barrageIndex].onShow) {
+      return
+    }
+    this.barrages.push(barrageIndex)
+    barrageList[barrageIndex].onShow = true
+  }
+
+  removeBarrage(barrageIndex) {
+    var index = barrageIndex
+    this.barrages.splice(index, 1)
+    barrageList[barrageIndex].onShow = false
+  }
+}
+
+// TODO:实现弹幕效果
+
+var barrageList = []
+var barrageTracks = reactive([])
+var barrageAmount = barrageList.length // 弹幕总数
+var onTrackAmount = 0 // 当前正在播放的弹幕数
+
+/* 获取一条弹幕 */
+async function fetchBarrage() {
+  return fetch('https://v1.hitokoto.cn')
+    .then((response) => response.json())
+    .then((data) => data.hitokoto)
+}
+
+/* 初始化弹幕列表 */
+async function loadBarrageList() {
+  for (let i = 0; i < 10; i++) {
+    barrageList.push(new Barrage('这是第' + i + '条弹幕', false))
+  }
+  for (let i = 0; i < 0; i++) {
+    fetchBarrage().then((text) => {
+      barrageList.push(new Barrage(text, false))
+    })
+  }
+  console.log('加载完毕')
+}
+
+loadBarrageList().then(() => {
+  barrageAmount = barrageList.length
+  addSomeBarrage()
+  setInterval(() => {
+    setTimeout(
+      () => {
+        addSomeBarrage()
+      },
+      3000 * Math.random() + 100,
+    )
+  }, 5000)
+})
+
+function findRandomTrack() {
+  if (barrageTracks.length == 0) {
+    return null
+  }
+  var trackIndex = Math.floor(Math.random() * barrageTracks.length)
+  return barrageTracks[trackIndex]
+}
+
+function createTrack() {
+  if (barrageTracks.length >= barrageAmount) {
+    return findRandomTrack()
+  }
+  var trackId = 'track-' + Math.floor(Math.random() * 1000000)
+  var track = new BarrageTrack(trackId)
+  barrageTracks.push(reactive(track))
+  return track
+}
+
+function findTrack(trackId) {
+  return barrageTracks.find((track) => track.id === trackId)
+}
+
+function removeTrack(trackId) {
+  var trackIndex = barrageTracks.findIndex((track) => track.id === trackId)
+  var track = findTrack(trackId)
+  if (track.barrages.length > 0 && track.barrage_waiting > 0) {
+    return
+  }
+  if (trackIndex > -1) {
+    barrageTracks.splice(trackIndex, 1)
+  }
+}
+
+function createBarrage(barrageId) {
+  if (onTrackAmount >= barrageAmount) return
+  var track = null
+  if (barrageTracks.length == 0 || Math.random() <= 0.6) {
+    track = createTrack()
+  } else {
+    // 随机选择一个已有的轨道
+    track = findRandomTrack()
+  }
+  // 将弹幕加入轨道
+  if (!track) {
+    return
+  }
+  track.barrage_waiting++
+  onTrackAmount++
+  // 延时加入，避免弹幕重叠
+  setTimeout(
+    () => {
+      track.sendBarrage(barrageId)
+    },
+    2000 * Math.random() + 100,
+  )
+}
+
+/* 将一些弹幕重新加入轨道 */
+function addSomeBarrage() {
+  // 生成随机的数量
+  var sendAmount = Math.floor(Math.random() * (barrageAmount - onTrackAmount)) + 1
+  // 不多于40%的弹幕
+  if (sendAmount > Math.floor(barrageAmount * 0.3)) {
+    sendAmount = Math.floor(barrageAmount * 0.3)
+  }
+  for (let i = 0; i < sendAmount; i++) {
+    // 抽一条未显示的弹幕
+    var barrageId = -1
+    for (let j = 0; barrageId == -1; j++) {
+      // 防溢出
+      if (j >= barrageAmount) {
+        j = 0
+      }
+      if (!barrageList[j].onShow) {
+        // 70%的可能性抽中此弹幕
+        if (Math.random() >= 0.3) continue
+        barrageId = j
+        break
+      }
+    }
+    if (barrageId == -1) {
+      break
+    }
+    createBarrage(barrageId)
+  }
+}
+
+/* 弹幕显示结束之后的处理 */
+function animationend(trackId, barrageId) {
+  var track = findTrack(trackId)
+  // 将弹幕从trackId的轨道中移除
+  if (track == null) return
+  track.removeBarrage(barrageId)
+  onTrackAmount--
+  // 如果轨道中没有弹幕了，就删除这个轨道
+  removeTrack(trackId)
+
+  addSomeBarrage()
+}
+</script>
+
+<template>
+  <div v-for="track in barrageTracks" :key="track.id" class="track">
+    <div
+      v-for="barrageId in track.barrages"
+      :key="barrageId"
+      class="barrage"
+      @animationend="animationend(track.id, barrageId)"
+    >
+      {{ barrageList[barrageId].text }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+template {
+  /* track应当自动地纵向排列 */
+  display: flex;
+  flex-direction: column;
+}
+
+.track {
+  position: relative;
+  height: 24px;
+  overflow: hidden;
+  transition: all 0.3s;
+}
+
+.barrage {
+  position: absolute;
+  white-space: nowrap;
+  color: white;
+  font-size: 16px;
+  /* font-weight: bold; */
+  text-shadow: 0px 0px 4px #000000aa;
+  animation: move-r2l linear 10s;
+  user-select: none;
+}
+
+@keyframes move-r2l {
+  from {
+    left: 100%;
+    transform: translateX(0);
+  }
+  to {
+    left: 0;
+    transform: translateX(-100%);
+  }
+}
+</style>
